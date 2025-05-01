70c998cac9bdff4fff72c73a11cb0d2cb5b7be81
alectunbridge <alec.tunbridge@autotrader.co.uk>
2025-05-01 15:51:59 +0100

    initial commit

---
+++ src/main/java/com/example/helloworld/HelloworldApplication.java
@@ -0,0 +1,25 @@
+
+package com.example.helloworld;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class HelloworldApplication {
+
+	@RestController
+	class HelloworldController {
+		@GetMapping("/")
+		String hello(@RequestParam String name) {
+			return "Hello " + name + "!";
+		}
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldApplication.class, args);
+	}
+}
