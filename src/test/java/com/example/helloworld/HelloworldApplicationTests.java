91fce0471637ecf4fbfae27e9e97cafbe3ef11df
alec.tunbridge <alec.tunbridge@autotrader.co.uk>
2025-05-06 11:25:51 +0100

    Fix test to check Heroku CI

--- src/test/java/com/example/helloworld/HelloworldApplicationTests.java
+++ src/test/java/com/example/helloworld/HelloworldApplicationTests.java
@@ -3,14 +3,14 @@ package com.example.helloworld;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class HelloworldApplicationTests {
 
 	@Test
 	void contextLoads() {
-		fail("This will always fail!");
+		assertEquals( 1 + 1, 2 );
 	}
 
 }
