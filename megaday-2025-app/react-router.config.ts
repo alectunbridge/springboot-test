a69a87606dfd5dbb52a7b41ecf349bf5f9da97ee
alec.tunbridge <alec.tunbridge@autotrader.co.uk>
2025-05-28 10:07:04 +0100

    Add yarn and node dist to gitignore
    Attempt 6

---
+++ megaday-2025-app/react-router.config.ts
@@ -0,0 +1,7 @@
+import type { Config } from "@react-router/dev/config";
+
+export default {
+  // com.example.helloworld.Config options...
+  // Server-side render by default, to enable SPA mode set this to `false`
+  ssr: false,
+} satisfies Config;
