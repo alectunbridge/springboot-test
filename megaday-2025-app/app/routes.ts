7115824be8b4f9d01ab79da7de2cd1dc2256028d
Steve Worsley <steve.worsley@autotrader.co.uk>
2025-06-04 11:23:38 +0100

    feat: add pico css framework
    This commit removes tailwind and
    replaces it with a lightweight
    css framework called pico.
    
    It also adds a style-guide page
    with a couple of examples of
    components.

--- megaday-2025-app/app/routes.ts
+++ megaday-2025-app/app/routes.ts
@@ -4,5 +4,5 @@ export default [
     // route("/", "routes/home.tsx"),
     route("", "routes/home.tsx"),
     route("callapi", "callapi/callapi.tsx"),
-
+    route("style-guide", "routes/style-guide/style-guide.tsx"),
 ] satisfies RouteConfig;
